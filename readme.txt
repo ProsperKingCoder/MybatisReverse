1f5114d3db744029c2ca7baaef3c749392a85ace
ProsperKingCoder <397989539@qq.com>
2018-10-12 10:48:13 +0800

    maven版mybatis逆向工程

---
+++ readme.txt
@@ -0,0 +1,5 @@
+注:maven版mybatis逆向工程
+
+1.将项目导入eclipse中
+2.项目右键->run as->maven build...,
+Goals:中输入mybatis-generator:generate
---
+++ src/main/java/com/zengyikeji/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package com.zengyikeji.dao;
+
+import com.zengyikeji.entities.User;
+import java.util.List;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    User selectByPrimaryKey(String id);
+
+    List<User> selectAll();
+
+    int updateByPrimaryKey(User record);
+}
